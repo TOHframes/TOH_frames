be377f8d6b9923d840913cf03f8a28d8adfa3704
TOHframes <152864958+TOHframes@users.noreply.github.com>
2026-04-07 16:16:20 +0200

    Update back.js

--- HarryPotterGacha/back.js
+++ HarryPotterGacha/back.js
@@ -1,20 +1,14 @@
-// ====================== PRZYCISK BACK ======================
     backButton.addEventListener('click', function () {
-        // 1. Ukrywamy ekran z kartami
         cards_you_got.style.display = "none";
 
-        // 2. Pokazujemy paczkę
         packWrapper.style.display = "block";
 
-        // 3. Resetujemy pełną paczkę
         fullImg.style.opacity = '1';
         fullImg.style.transition = '';
 
-        // 4. NAJWAŻNIEJSZE – czyścimy inline style, który blokował hover
         packWrapper.style.transform = '';
-        packWrapper.style.removeProperty('transform');   // dodatkowe zabezpieczenie
+        packWrapper.style.removeProperty('transform');
 
-        // 5. Usuwamy klasę blokującą
         packWrapper.classList.remove('opening');
 		packWrapper.classList.remove("no-shadow");
     });
