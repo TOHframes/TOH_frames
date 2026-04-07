65eee66c8176bea170494ea59aa759874b229591
TOHframes <152864958+TOHframes@users.noreply.github.com>
2026-04-07 16:19:32 +0200

    Update modal2.js

--- HarryPotterGacha/modal2.js
+++ HarryPotterGacha/modal2.js
@@ -1,6 +1,3 @@
-
-
-// ====================== MODAL – KLIKNIĘCIE W KARTĘ ======================
         function openCardModal(card, id) {
 			
 			let data = JSON.parse(localStorage.getItem(id));
@@ -77,17 +74,17 @@
 
             modalContent.appendChild(enlarged);
 
-            // Kliknięcie w powiększoną kartę = przekierowanie do Wikipedii
+
             enlarged.addEventListener('click', function (e) {
                 e.stopImmediatePropagation();
-                const wikiUrl = card.dataset.wiki || 'https://pl.wikipedia.org/wiki/C%2B%2B';
+                const wikiUrl = card.dataset.wiki;
 				window.open(wikiUrl, '_blank');
             });
 
             modalOverlay.style.display = 'flex';
         }
 
-        // Kliknięcie poza powiększoną kartą = zamknięcie
+
         modalOverlay.addEventListener('click', function (e) {
             if (e.target === modalOverlay) {
                 modalOverlay.style.display = 'none';
@@ -95,7 +92,7 @@
             }
         });
 
-        // Przypisanie kliknięcia do wszystkich kart (działa nawet po pokazaniu cards_you_got)
+
         document.addEventListener('DOMContentLoaded', () => {
             document.querySelectorAll('.collection_name_modal').forEach(card => {
                 card.style.cursor = 'pointer';
